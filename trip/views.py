6acd5ce43f39e069365728fe3c4dcfde294f0790
wayne chan <waynestock666@gmail.com>
2024-04-09 03:24:39 +0800

    update

--- trip/views.py
+++ trip/views.py
@@ -54,10 +54,7 @@ class TripCreateView(CreateView):
         form.instance.owner = self.request.user
         return super().form_valid(form)
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['google_maps_api_key'] = os.getenv('GOOGLE_MAPS_API_KEY')
-        return context
+    
     
     
 class TripDetailView(DetailView):
