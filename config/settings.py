dd1cbf8cc6011efb3ed8c132d96ea40271462a18
wayne chan <waynestock666@gmail.com>
2024-04-09 02:05:25 +0800

    update

--- config/settings.py
+++ config/settings.py
@@ -126,9 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # 添加这行，设定查找静态文件的目录
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
